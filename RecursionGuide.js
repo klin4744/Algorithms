7c33ae7d5d41686490bc354a762a66c5307646b5
klin4744 <klin4744@bths.edu>
2018-12-01 02:35:59 -0500

    Update RecursionGuide.js

--- RecursionGuide.js
+++ RecursionGuide.js
@@ -73,3 +73,80 @@ function factorialR(num) {
 // 2. Returning the wrong thing - For example returning num * factorial(num), we will never hit our base case and cause the recursion to hit the max call size. Make sure that you return something that can resolve.
 
 // If a call stack is full, we call this a Stack overflow! this happens if there is either no base case or the wrong thing is returned.
+
+/////////////////////////////
+// Helper Method Recursion //
+////////////////////////////
+
+// We have a main function which calls a helper function which calls itself
+
+function outer(input) {
+  var outerScopedVariable = [];
+  function helper(helperInput) {
+    // modify the outer scoped varibled
+    helper(helperInput--);
+  }
+  helper(input);
+  return outerScopedVariable;
+}
+
+// Real example, collect all the odd values in an array
+function collectOddValues(arr) {
+  let result = []; // initialize our answer array
+  function helper(helperInput) {
+    //initialize helper function
+    // The helper function just checks for odd numbers and pushes them into our intialized empty array
+    if (helperInput.length === 0) {
+      return; //Our base case, if we recieve an empty array, exit execution
+    }
+    if (helperInput[0] % 2 !== 0) {
+      result.push(helperInput[0]);
+    }
+    helper(helperInput.slice(1)); // We call array again with a new array that doesnt have the 0 index
+
+    // arr.slice returns a copy of an array from arg1 = begining, arg2 = end. slice(1) here returns the array we passed in without its 0 index element.
+  }
+  helper(arr); // call our helper function and pass in our input
+  return result;
+}
+
+//////////////////////
+// *Pure Recursion* //
+//////////////////////
+
+// Recursion without using a helper function
+
+function collectOddValuesP(arr) {
+  let newArr = []; // Initialize a answer/ empty variable
+
+  // Set the base case, if the arr length passed into our collectOddValuesP function is 0, exit the function
+
+  if (arr.length === 0) {
+    return newArr;
+  }
+
+  // The coding logic part, check if the first item in the array passed into our function is odd, if it is, push it into our newArr intialized at the tep
+  if (arr[0] % 2 !== 0) {
+    newArr.push(arr[0]);
+  }
+
+  newArr = newArr.concat(collectOddValues(arr.slice(1)));
+  return newArr;
+}
+// Calling our function again, newArr.concat allows us to append array values to newArr. collectOddValues, our original function is called here but we are passing in the input array minus the first item (we are making a copy from the first index to the end then passing it in)
+
+//Everything we loop, we're creating more new arrays and pushing the 0 index into the new array if the 0 index of the input is odd
+// We then are concatenating this newArray or array containing only the one odd number to the array directly on the stack below it
+
+// Example : collectOddValues([3,1,9,10,1])
+// Call stack 1 : newArr = [3] => our first number is odd so it is added to new array
+// now newArr = newArr.concat(collectOddvalues([1,9,10,1]))
+// Call stack 2 : newArr = [1], first number is odd
+// Call stack 3: newArr = [9]
+// Call stack 4 : newArr = [], first number is not odd, nothing is pushed
+// Call stack 5: newArr = [1]
+// Call stack 6: return is called immediately since array has no length
+// Call stack 5 => newArr = [1].concat([]) = [1]
+// Call stack 4 => newArr = [].concat([1]) = [1]
+// CS 3 => newArr = [9].concat([1]) = [9,1];
+// CS 1 = newArr = [3].concat([1,9,1]) = [3,1,9,1]
