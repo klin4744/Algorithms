20a91898c096f1632a3dff631fc79f239f9d6a02
klin4744 <klin4744@bths.edu>
2018-12-04 22:54:20 -0500

    Create SortingAlgorithmsOverview.js

---
+++ SortingAlgorithmsOverview.js
@@ -0,0 +1,29 @@
+// What is sorting?
+// The process of rearranging the items in a collection so that the items are in some kind of order
+
+// Examples:
+// Sorting numbers from smallest to largest
+// Sorting names alphabetically
+// Sorting movies based on release year
+// Sorting movies based on revenue
+
+function sort(arr) {
+  return arr;
+}
+
+sort([23, 45, 6, 12, 13]); // [6,12,13,23]
+
+// Why learn sorting?
+// Very common in coding
+// Every sorting algorithm has different pros or cons, they work better in different scenarios (different kinds of data)
+// We also want to understand how built in sort methods work, so we know when we should use them over another!
+// Classic interview problems!
+
+// Elementary sorting algorithms //
+// Bubble Sort //
+// Selection Sort //
+// Insertion Sort //
+
+// These sorting methods are not common but are a good introduction to sorting.
+
+// Continue in bubbleSort.js file!
