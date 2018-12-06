75f8a867280ed18da8bd15fd636203cafacd3b50
klin4744 <klin4744@bths.edu>
2018-12-06 01:03:16 -0500

    Update insertionSort.js

--- insertionSort.js
+++ insertionSort.js
@@ -23,13 +23,19 @@
 // Continue to the next element and if it is in the incorrect order, iterated through the sorted potion and place it in the correct place
 // Return array
 
+// We first loop through the array, starting with index one
+// We nest another loop which checks, for each number, if the number before it is larger than it, it is is than we swap its position with that number.
+// using the statement j >= 0, makes us loop the entire length before the current number we're at
+// the statement && arr[j] > currentVal makes sure we ONLY swap if a number before our current number if larger than our current number and also stops the loop if we bump into a number less than our current j iteration. Essentially what happens is our j loop is shifting all the elements that are larger than it one unit to the right, when it bumps into a value that it is larger than, we stop looping the j loop, and place the current value right before the number that the current value is larger than
 function insertionSort(arr) {
   for (let i = 1; i < arr.length; i++) {
-    for (let j = i - 1; j >= 0; j--) {
-      if (arr[j] > arr[i]) {
-        [arr[j], arr[i]] = [arr[i], arr[j]];
-      }
+    let currentVal = arr[i];
+    for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+      arr[j + 1] = arr[j];
     }
+    arr[j + 1] = currentVal;
   }
   return arr;
 }
+
+// Note, we are using var instead of let because var has global scope and is accessible outside of our nested loop
