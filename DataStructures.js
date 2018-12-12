d5d93b40caa3b9e12584ad72c1c28ce830da017d
klin4744 <klin4744@bths.edu>
2018-12-12 00:56:53 -0500

    Created DataStructures,js and added Introduction section

---
+++ DataStructures.js
@@ -0,0 +1,34 @@
+// Data Structures //
+
+// INTRODUCTION //
+
+// Data strcutures are collections of values, the relationships among them, and the functions or operations that can be applied to the data
+
+// Holds data, stores the data in a particular pattern, and has their OWN methods.
+
+// Why are there so many data structures?
+
+// Some data structures are really specifialized while others, like arrays, are more generally used
+// They all do different things
+// Usage is important
+
+// Basically ways of storing data, based on some kind of relationship.
+
+// Why is it important?
+// Datastructures are VERY important in real life applications, using an array for every single type of data may not be the best.
+// Trees are used in DOM manipulation for example.
+
+// There is NO best data structure, all excel in different situations
+
+// For example you might want to use a graph data structure if you're trying to build something like google maps
+// Would be very difficult to do so using arrays
+
+// Need an ordered list with fast inserts/removals at the beginning and the end?
+// Use a linked list!
+
+// Need to web scrape nested HTML
+// Use a tree!
+
+// Task scheduler aka choose which tasks to prioritize
+// Use a binary heap
+////////////////////////////////////////////////////
