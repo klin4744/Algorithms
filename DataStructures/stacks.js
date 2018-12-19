672b61dddfa58c7d28388a4a0fb3a365beeb7e4e
klin4744 <klin4744@bths.edu>
2018-12-19 01:21:11 -0500

    Create stacks.js
    Introduction

---
+++ DataStructures/stacks.js
@@ -0,0 +1,6 @@
+////////////////////////////
+//// Stacks and Queues ////
+//////////////////////////
+
+// What is a stack?
+// A stack is a LIFO data structure. LIFO means that the last element added to the stack will be the first element removed from the stack.
