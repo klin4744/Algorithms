eebe0b53f5f2b48243991a38ca075007a93359a2
klin4744 <klin4744@bths.edu>
2018-12-31 09:38:09 -0500

    Update HashTables.js

--- DataStructures/HashTables.js
+++ DataStructures/HashTables.js
@@ -194,3 +194,17 @@ class HashTable {
     return returnArray;
   }
 }
+// Hash Table Big O
+// Insert: O(1)
+// Deletion: O(1)
+// Access: O(1)
+
+// Recap
+// Hash tables are collections of key-value pairs
+// Hash tables can find values quickly given a key
+// Hash tables can add new key-values quickly
+// Hash tables store data in a large array, and work by hashing the keys
+// A good hash should be fast, distribute keys uniformly, and be deteministic
+// Separate chaining and linear probing are two strategies used to deal with two keys that hash to the same index
+// Separate chaining - put duplicates in the same slot nested
+// Linear probing - Look ahead for the next availible slot
