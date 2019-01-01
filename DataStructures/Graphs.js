19894f0dc73d55c8d4476264e5bfa14ef86a1015
klin4744 <klin4744@bths.edu>
2019-01-01 01:59:48 -0500

    Update Graphs.js

--- DataStructures/Graphs.js
+++ DataStructures/Graphs.js
@@ -20,3 +20,50 @@
 // Everywhere!
 
 // There are no patterns in graphs!
+
+// Types of graphs
+// Vertex - a node
+// Edge- connection between nodes
+// Weighted/unweighted - Unweighted graphs are graphs in which the edges do not have values. Weighted graphs have edges with assigned values that give some king of insight on the connection. For example, google maps would use a weighted graph because we would like to know the distance between two vertices (places).
+// Undirected/directed - Undirected graphs are graphs that have no direction associated to its edges. We can go from A to E or E to A. This is often useful for things such as Facebook friends, a two way relationship. Directed graphs are graphs that have directions associated to its edges, the edges are represented by arrows. This means  B <- A -> C, a can go to B and C but both C and B cannot go to anything, they are dead ends. Note: directed graphs can also be bi-directional.
+
+// Think about instagram, if you follow a celebrity you have access to their content but they will likely not follow you back and thus will not be able to see your content, this is a directed relationship.
+
+// A tree is a graph that only has one path to go to a specific node.
+
+// Representing a graph //
+
+// 1.
+// Adjacency matrix //
+// A matrix is a 2d structure with rows and columns, we can represent connections of a graph as a matrix
+// Example
+//     A       B         C
+// A   0       1         1
+// B   1       0         1
+// C   1       1         0
+
+// Here we have a simple triangle structure where A is linked to B and C, C is linked to B and A, and B is linked to A and C. By linked we mean connected.
+
+// 2.
+// Adjacency list //
+// For Adjacency lists, we simply just create an array which stores arrays that contain a vertex's connections
+// For Example:
+//    [
+// 0    [1,5],
+// 1    [0,2],
+// 2    [1,3],
+// 3    [2,4],
+// 4    [3,5],
+// 5    [4,0]
+//    ]
+// The node with the name 0 or corresponding to the 0 index, has edges connecting it to vertex 1 and vertex 5. etc.
+
+// In the case our nodes are indexes, we can instead use a hash table, aka an object in javascript
+// {
+//    A: ["B","F"] ,
+//    B: ["B","F"] ,
+//    C: ["B","F"] ,
+//    D: ["B","F"] ,
+//    E: ["B","F"] ,
+//    F: ["B","F"]
+// }
