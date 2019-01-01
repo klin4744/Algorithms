bcf1425b2735aa06297c12168f91288d5e5a8b7d
klin4744 <klin4744@bths.edu>
2019-01-01 01:30:24 -0500

    Create Graphs.js

---
+++ DataStructures/Graphs.js
@@ -0,0 +1,22 @@
+/////////////
+// Graphs //
+////////////
+
+// A graph consists of a finite set of vertices or nodes or points, together with a set of unordered pairs of these vertices for an undirected graph or set of ordered pairs for a directed graph.
+
+// A graph is simply a collection of nodes and connections between those nodes.
+
+// Example
+// https://www.sanfoundry.com/wp-content/uploads/2017/08/data-structure-questions-answers-graph-q5.png
+// Simply a bunch of nodes and a bunch of connections between those nodes
+// A tree is a type of a graph but graphs don't necessarily have parent nodes, every node is treated the same way, no special node.
+
+// Uses for graphs
+// Social networking - Graphs are used commonly for social networking to connect friends where nodes are people and there are several connections based on different criteria (same school, mutual friends, etc).
+// Location/mapping - Google maps for example uses graphs where specific points are nodes and roads etc are connections. Google maps calculates the fastest path using graphs to get from one location to another. Note that google map also takes a ton of things into account such as traffic, speed limit, etc.
+// Reccomendations - People also watched, you might also like, people you might know! All these reccomendations are made using graphs! Intersections of nodes can give us insight on what to reccomend.
+// Visual hierarchy - graphs can be used to make visual representations of visual graphs! This is done by using connections.
+// File system optimizations
+// Everywhere!
+
+// There are no patterns in graphs!
