dd28f8ff6613e79b6ccab3358818088775ad92b4
klin4744 <klin4744@bths.edu>
2019-01-06 02:11:53 -0500

    Create Dijkstra'sAlgorithm.js

---
+++ DataStructures/Dijkstra'sAlgorithm.js
@@ -0,0 +1,12 @@
+///////////////////////////
+// Dijkstra's Algorithm //
+/////////////////////////
+
+// Dijkstra's Algorithm finds the shortest path between two vertices on a graph
+
+// Dijkstra's Algorithm is still widely used in:
+// GPS
+// Network routing
+// Biology
+// Airline tickets
+// etc.
