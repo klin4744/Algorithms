6d2aacbe84f089c28a7208b659f6f31d51a485ae
klin4744 <klin4744@bths.edu>
2019-04-13 23:17:03 -0400

    Update isUnique.js

--- isUnique.js
+++ isUnique.js
@@ -1,7 +1,7 @@
 // Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
 
 // With additonal data structures:
-
+// Time complexity O(N);
 function isUnique(str) {
   const strObj = {};
   for (let i = 0; i < str.length; i++) {
@@ -9,6 +9,21 @@ function isUnique(str) {
       strObj.str[i] = 1;
     } else {
       return false;
+    }
+  }
+  return true;
+}
+
+// Without additional data structures:
+
+// Brute force
+// O(N^2) time but O(1) space;
+function isUnique2(str) {
+  for (let i = 0; i < str.length; i++) {
+    for (let j = i + 1; j < str.legnth; j++) {
+      if (str[i] === str[j]) {
+        return false;
+      }
     }
   }
   return true;
